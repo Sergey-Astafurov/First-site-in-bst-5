47f1dec8b008fca45d7a3e0615ea20d8710b1759
Sergey Astafurov <stupidsociety16@gmail.com>
2021-12-10 19:38:50 +0600

    all

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-for (let i = 0; i < mas.length; i++) {
-    const k = mas[i];
-    console.log(k);
-    
-}
+
+// let card = document.querySelectorAll('.card')
+// let cardImg = document.querySelectorAll('.card-img')
+// card.forEach(elem => {
+//     elem.addEventListener('mouseover', function(event){
+//         event = event.target ;
+//         event.style.transform = "translateY(-30px)";
+//         // cardImg.forEach(function (event) {
+//         //     event = event.target ;
+//         //     event.style.filter = 'grayscale(0)';
+        
+                
+//         // })
+//     })
+// });
